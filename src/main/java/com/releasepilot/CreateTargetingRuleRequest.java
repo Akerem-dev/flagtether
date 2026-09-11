3e61194790757676b74ea2bf0289d9ed88604065
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-09-11 16:30:24 -0700

    fix: preserve optional targeting priority default

--- src/main/java/com/releasepilot/CreateTargetingRuleRequest.java
+++ src/main/java/com/releasepilot/CreateTargetingRuleRequest.java
@@ -22,7 +22,6 @@ public class CreateTargetingRuleRequest {
   @NotNull(message = "is required")
   private Boolean serveEnabled;
 
-  @NotNull(message = "is required")
   @Min(value = 0, message = "must be greater than or equal to 0")
   private Integer priority;
 
